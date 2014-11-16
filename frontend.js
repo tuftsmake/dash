9971c8503812f74eb329096524ba92d14694de61
andybraren <andybraren@gmail.com>
2014-11-16 12:56:56 -0500

    Removed double map src switcher, cleaned up code
    Deleted extra stuff at the bottom related to changing the background,
    and commented out the double map src switcher function

--- frontend.js
+++ frontend.js
@@ -268,6 +268,7 @@ function parseDay(day){
 	return today;
 }
 
+/*
 function setMap(day, time) {
 	var joeyday = new Date();
 	var day = joeyday.getDay();
@@ -298,68 +299,4 @@ function setMap(day, time) {
         }
 	}
 }
-
-
-// Here are a few potentially useful links:
-
-// http://stackoverflow.com/questions/4358155/changing-background-based-on-time-of-day-using-javascript
-
-// http://en.kioskea.net/faq/27175-javascript-change-greeting-based-on-time-of-day
-
-// http://stackoverflow.com/questions/12223286/load-a-different-link-based-on-time-of-the-day
-
-/*
-var done = true,
-    fading_div = document.getElementById('fading_div'),
-    fade_in_button = document.getElementById('fade_in'),
-    fade_out_button = document.getElementById('fade_out');
-
-function function_opacity(opacity_value) {
-    fading_div.style.opacity = opacity_value / 100;
-    fading_div.style.filter = 'alpha(opacity=' + opacity_value + ')';
-}
-
-function function_fade_out(opacity_value) {
-    function_opacity(opacity_value);
-    if (opacity_value == 1) {
-        fading_div.style.display = 'none';
-        done = true;
-    }
-}
-
-function function_fade_in(opacity_value) {
-    function_opacity(opacity_value);
-    if (opacity_value == 1) {
-        fading_div.style.display = 'block';
-    }
-    if (opacity_value == 100) {
-        done = true;
-    }
-}
-// fade in button
-fade_in_button.onclick = function () {
-    if (done && fading_div.style.opacity !== '1') {
-        done = false;
-        for (var i = 1; i <= 100; i++) {
-            setTimeout((function (x) {
-                return function () {
-                    function_fade_in(x)
-                };
-            })(i), i * 10);
-        }
-    }
-};
-// fade out button
-fade_out_button.onclick = function () {
-    if (done && fading_div.style.opacity !== '0') {
-        done = false;
-        for (var i = 1; i <= 100; i++) {
-            setTimeout((function (x) {
-                return function () {
-                    function_fade_out(x)
-                };
-            })(100 - i), i * 10);
-        }
-    }
-};
 */
