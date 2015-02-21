3ed54aa18bb802911bf2a2021273ed2f0c196f0d
andybraren <andybraren@gmail.com>
2015-02-20 21:21:00 -0500

    Removed broken nightmode cookie

--- frontend.js
+++ frontend.js
@@ -84,27 +84,7 @@ function killIntro(){
 /* NIGHT MODE
 -------------------------------------------------- */
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
-    }
-    return "";
-}
-
-window.onload = function checkCookie() {
-    var setting = getCookie("nightmode");
-    if (setting == "night") {
-      nightmode();
-    } else {
-    }
-}
-
 function nightmode() {
-  document.cookie="nightmode=night;path=/";
   $(".bubbletitle").toggleClass("night");
   $(".bubblecontent").toggleClass("night");
   $("html").toggleClass("night");
