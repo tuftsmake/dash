77b4baa9019c66b852e03acdb71fe905cfb340b2
Charles Meyer <charles.meyer@tufts.edu>
2015-02-28 13:49:17 -0500

    some dining refactoring

--- dining.js
+++ dining.js
@@ -1,5 +1,24 @@
 // Dining.js - gets dining data for dewick and carm
 
+function build_dining_preview(data){
+    $(".dining_loading").hide();
+    dewick_entrees = data['dewick']['Dinner']['Dinner Entrees'];
+    dewick_entrees.forEach(function(elem){
+        $('<tr><td class="dining_menu_item">' + elem + '</td></tr>').appendTo("#dewick_table");
+    })
+    carm_entrees = data['carm']['Dinner']['Dinner Entrees'];
+    carm_entrees.forEach(function(elem){
+       $('<tr><td class="dining_menu_item">' + elem + '</td></tr>').appendTo("#carm_table");
+    })
+}  
+
+function build_dining_full_bubble(data){
+    dewick_data = data['dewick'];
+    for (var meal in data['dewick']){
+        console.log(meal);
+    }
+}
+
 var refresh_dining = function()
 {
     $(".dining_loading").show();
@@ -11,27 +30,14 @@ var refresh_dining = function()
     $.ajax({
         url: 'http://tuftsdiningapi.appspot.com/api/', 
         success: function(data){
-                    $(".dining_loading").hide();
-                    // foods = $.parseJSON(data);
-                    dentrees = data['dewick']['Dinner']['Dinner Entrees'];
-                    dentrees.forEach(function(elem){
-                       $( '<tr><td class="dining_menu_item">' + elem + "</td></tr>" ).appendTo("#dewick_table");
-                    })
-                    $("<br>").appendTo("#")
-                    centrees = data['carm']['Dinner']['Dinner Entrees'];
-                    centrees.forEach(function(elem){
-                       $( '<tr><td class="dining_menu_item">' + elem + "</td></tr>" ).appendTo("#carm_table");
-                    })
+                    build_dining_preview(data);
+                    build_dining_full_bubble(data);
                 },
 
         error: function(data){
                     $(".dining_loading").value = "Failed to load"
               }
     });
-}  
-
-var populate_dining = function() {
-
 }
 
 $(document).ready(refresh_dining);
